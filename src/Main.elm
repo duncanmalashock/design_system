9f5987b6e2c1f5fbc384516b91f20102f9456c53
Duncan Malashock <duncanmalashock@gmail.com>
2017-11-07 21:56:59 -0500

    Initial commit

---
+++ src/Main.elm
@@ -0,0 +1,50 @@
+module Main exposing (..)
+
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (class)
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+type alias Model =
+    {}
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+initialModel : Model
+initialModel =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( initialModel, Cmd.none )
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "app" ]
+        [ text "Hello world!" ]
